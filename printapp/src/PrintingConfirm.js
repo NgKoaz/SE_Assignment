c25c44fecb08cffa2608b873fc2646943c90c95e
NgKoaz <nguyenkhoa220703@gmail.com>
2023-11-15 20:58:46 +0700

    Update

--- printapp/src/PrintingConfirm.js
+++ printapp/src/PrintingConfirm.js
@@ -2,7 +2,60 @@ import React from 'react'
 
 const PrintingConfirm = ({step, setStep}) => {
   return (
-    <div>PrintingConfirm</div>
+    <div className="absolute top-1/4 left-1/4 w-1/2 h-auto">
+        <div className="bg-blue-100 rounded-lg p-3 w-full">
+            <h1 className="text-3xl font-semibold mb-4">Hóa đơn</h1>
+            <hr className="mt-5 mb-5 border-gray-300" />
+
+            <div>
+                <label className="text-xl font-semibold">Địa điểm máy in</label>
+                <br />
+
+                <label className="text-xl font-semibold">Hướng giấy</label>
+                <br />
+
+                <label className="text-xl font-semibold">Loại giấy</label>
+                <br />
+
+                <label className="text-xl font-semibold">Số lượng trang cho mỗi bản sao</label>
+                <br />
+
+                <label className="text-xl font-semibold">Tổng số bản sao cần in</label>
+                <br />
+
+                <label className="text-xl font-semibold">Tổng số trang cần in/label</label>
+                <br />
+
+                <label className="text-xl font-semibold">Tổng số trang hiện tại</label>
+                <br />
+
+                <label className="text-xl font-semibold">Còn lại</label>
+                <br />
+            </div>
+            <div className="flex flex-col gap-2 items-center">
+                <button 
+                    value="continue"
+                    className="bg-indigo-600 w-2/5 h-12 rounded-2xl text-white text-xl font-semibold">
+                        
+                    Tiến hành in
+                </button>
+                <button 
+                    value="continue"
+                    className="bg-purple-600 w-2/5 h-12 rounded-2xl text-white text-xl font-semibold">
+                    
+                    Mua thêm giấy
+                </button>
+                <button 
+                    value="continue"
+                    className="bg-red-600 w-2/5 h-12 rounded-2xl text-white text-xl font-semibold">
+                    
+                    Quay về
+                </button>
+            </div>
+        </div>
+
+        
+    </div>  
   )
 }
 
--- printapp/src/Uploader.js
+++ printapp/src/Uploader.js
@@ -13,45 +13,45 @@ const Uploader = ({ step, setStep }) => {
 
   return (
     <div className="h-1/2 w-1/2 absolute top-1/4 left-1/4 bg-gray-100">
-      <div className="h-1/6 w-full bg-white shadow-lg drop-shadow-xl rounded-lg flex flex-row justify-center">
-        <button className="h-3/5 w-32 text-lg font-semibold text-white bg-indigo-600 rounded-full">Tải file mới</button>
-        <button className="h-3/5 w-32 bg-white rounded-full">File đã tải</button> 
-      </div>
-      <form 
-        action="" 
-        className="flex justify-center items-center border-2 border-dashed border-indigo-600 h-4/6 w-full cursor-pointer"
-        onClick={() => document.querySelector(".input-field").click()}  
-      >
-        <input 
-          type="file" 
-          accept="" 
-          className="input-field" 
-          hidden
-          onChange={({target: {files}}) => {
-            files[0] && setFileName(files[0].name)
-            if (files){
-              setFile(URL.createObjectURL(files[0]))
-            }
-          }}
-        />
+        <div className="h-1/6 w-full bg-white shadow-lg drop-shadow-xl rounded-lg flex flex-row justify-center">
+            <button className="h-3/5 w-32 text-lg font-semibold text-white bg-indigo-600 rounded-full">Tải file mới</button>
+            <button className="h-3/5 w-32 bg-white rounded-full">File đã tải</button> 
+        </div>
+        <form 
+            action="" 
+            className="flex justify-center items-center border-2 border-dashed border-indigo-600 h-4/6 w-full cursor-pointer"
+            onClick={() => document.querySelector(".input-field").click()}  
+        >
+            <input 
+            type="file" 
+            accept="" 
+            className="input-field" 
+            hidden
+            onChange={({target: {files}}) => {
+                files[0] && setFileName(files[0].name)
+                if (files){
+                setFile(URL.createObjectURL(files[0]))
+                }
+            }}
+            />
 
-        {file ? 
-        (<div>{fileName}</div>) 
-        : 
-        <>
-          <MdCloudUpload color='#1475cf' size={60} />
-          <br />
-          <p>Bấm vào để tải file</p>
-        </>
-        }
-      </form>
-      <div className="bg-white h-1/6 w-full flex items-center justify-center">
-        <button 
-          onClick={e => handleSubmit(e)}
-          className="bg-indigo-600 h-1/2 w-1/4 text-xl font-semibold text-white rounded-2xl">
-          Tiếp tục
-        </button>
-      </div>
+            {file ? 
+            (<div>{fileName}</div>) 
+            : 
+            <>
+            <MdCloudUpload color='#1475cf' size={60} />
+            <br />
+            <p>Bấm vào để tải file</p>
+            </>
+            }
+        </form>
+        <div className="bg-white h-1/6 w-full flex items-center justify-center">
+            <button 
+            onClick={e => handleSubmit(e)}
+            className="bg-indigo-600 h-1/2 w-1/4 text-xl font-semibold text-white rounded-2xl">
+            Tiếp tục
+            </button>
+        </div>
     </div>
   )
 }
