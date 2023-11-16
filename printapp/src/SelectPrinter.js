cc93db0a64dea8f29ddfbc935e664d8aa18dd8ea
NgKoaz <nguyenkhoa220703@gmail.com>
2023-11-16 23:17:08 +0700

    Update

--- printapp/src/SelectPrinter.js
+++ printapp/src/SelectPrinter.js
@@ -17,59 +17,59 @@ const SelectPrinter = ({ step, setStep }) => {
         }
     }
 
-    function handleForm(e){
-        e.preventDefault()
-        console.log(e.target.value)
-        if (e.target.value === 'back')
-            setStep(step - 1)
-        else {
-            setStep(step + 1)
-        }
-    }
-
   return (
     <div className="absolute top-1/4 left-1/4 w-1/2 h-2/3 ">
         <form className="w-full">
             <div className="bg-blue-100 rounded-lg p-3 w-full h-2/3">
-                <label className="text-xl font-semibold">Printer</label>
-                <select>
-                    <option>Chọn ngẫu nhiên</option>
-                </select>
+                <div className="grid gird-cols-1 gap-5">
+                    <label className="text-xl font-semibold">Printer</label>
+                    <select className="w-72">
+                        <option>Chọn ngẫu nhiên</option>
+                    </select>
+                </div>
                 <br />
 
-                <label className="text-xl font-semibold">Số lượng bản sao</label>
-                <input 
-                    type="text" 
-                    
-                    className="text-base border-2 rounded-lg w-1/2 h-12 mt-2 mb-2 p-2 hover:border-gray-600"
-                />
+                <div className="flex justify-between">
+                    <label className="text-xl font-semibold">Số lượng bản sao</label>
+                    <input 
+                        type="text" 
+                        
+                        className="text-base border-2 rounded-lg w-1/2 h-12 mt-2 mb-2 p-2 hover:border-gray-600"
+                    />
+                </div>
                 <br />
 
-                <label className="text-xl font-semibold">Hướng giấy</label>
-                <ul className="flex flex-auto">
-                    <li>
-                        <input type="radio" name="orientation" id="portrait" value="portrait" />
-                        <label for="portrait">Dọc</label>
-                    </li>
-                    <li>
-                        <input type="radio" name="orientation" id="landscape" value="landscape" />
-                        <label for="landscape">Ngang</label>
-                    </li>
-                </ul>
+                <div className="flex justify-between">
+                    <label className="text-xl font-semibold">Hướng giấy</label>
+                    <ul className="flex flex-auto">
+                        <li>
+                            <input type="radio" name="orientation" id="portrait" value="portrait" />
+                            <label for="portrait">Dọc</label>
+                        </li>
+                        <li>
+                            <input type="radio" name="orientation" id="landscape" value="landscape" />
+                            <label for="landscape">Ngang</label>
+                        </li>
+                    </ul>
+                </div>
 
-                <label className="text-xl font-semibold">Khổ giấy</label>
-                <ul className="flex flex-auto">
-                    <li>
-                        <input type="radio" name="paper-size" id="a4" value="a4" />
-                        <label for="a4">A4</label>
-                    </li>
-                    <li>
-                        <input type="radio" name="paper-size" id="a3" value="a3" />
-                        <label for="a3">A3</label>
-                    </li>
-                </ul>
+                <div className="flex justify-between">
+                    <label className="text-xl font-semibold">Khổ giấy</label>
+                    <ul className="flex flex-auto">
+                        <li>
+                            <input type="radio" name="paper-size" id="a4" value="a4" />
+                            <label for="a4">A4</label>
+                        </li>
+                        <li>
+                            <input type="radio" name="paper-size" id="a3" value="a3" />
+                            <label for="a3">A3</label>
+                        </li>
+                    </ul>
+                </div>
+
             </div>
         </form>
+
         <div className="buttons flex flex-row justify-around mt-5">
             <button 
                 id="back"
@@ -84,6 +84,7 @@ const SelectPrinter = ({ step, setStep }) => {
                 Tiếp tục
             </button>
         </div>
+
     </div> 
   )
 }
--- printapp/tailwind.config.js
+++ printapp/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js, html}'],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '500px': '500px',
+      }
+    },
   },
   plugins: [],
 }
